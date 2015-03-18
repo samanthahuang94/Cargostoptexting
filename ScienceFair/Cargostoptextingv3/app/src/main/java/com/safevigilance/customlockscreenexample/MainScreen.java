3e8869bdd6398dc6c957c39b2a8b7301c0e447fd
Harry <harry.chan@hts.on.ca>
2015-03-17 23:06:04 -0400

    March 17

--- ScienceFair/Cargostoptextingv3/app/src/main/java/com/safevigilance/customlockscreenexample/MainScreen.java
+++ ScienceFair/Cargostoptextingv3/app/src/main/java/com/safevigilance/customlockscreenexample/MainScreen.java
@@ -2,31 +2,49 @@ package com.safevigilance.customlockscreenexample;
 
 import android.app.Activity;
 import android.app.ActivityManager;
-import android.app.KeyguardManager;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.PixelFormat;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.location.LocationProvider;
+import android.location.SettingInjectorService;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.provider.Settings;
 import android.util.Log;
-import android.view.KeyEvent;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
 import android.view.WindowManager;
 import android.widget.TextView;
 import android.widget.Button;
 
+import com.safevigilance.customlockscreenexample.HomeLocker;
 
-public class MainScreen extends Activity {
+public class MainScreen extends Activity{
 
     private LocationManager lm;
     private LocationListener ll;
+    private HomeLocker mHomeLocker;
+
     double mySpeed, maxSpeed;
 
     //Disabling the hard buttons
     @Override
     public void onBackPressed() {
     }
+
+
+
+
 
 
     @Override
@@ -49,6 +67,8 @@ public class MainScreen extends Activity {
             }
         });
 
+
+
         final TextView msg = (TextView) findViewById(R.id.currSpeedText);
         final TextView kph = (TextView) findViewById(R.id.LocationServicesError2);
 
@@ -66,7 +86,57 @@ public class MainScreen extends Activity {
             startService(serviceIntent);
 
         }
+
+        mHomeLocker = new HomeLocker();
+
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main_screen, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        if(id == R.id.action_settings){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mHomeLocker.unlock();
+        mHomeLocker = null;
+    }
+
+    public void showAlert(View view){
+        AlertDialog.Builder myAlert = new AlertDialog.Builder(this);
+        AlertDialog.Builder alert = myAlert;
+        alert.setMessage("To unlock your phone we need to acquire the speed of your device\n");
+        alert.setTitle("Please turn on High Accuracy location services");
+        alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        alert.setPositiveButton("Give Permission", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+
+            }
+        }); AlertDialog alertDialog = alert.create();
+        myAlert.show();
+
+    }
+
+
 
     private boolean isMyServiceRunning(Class<?> serviceClass) {
         ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
@@ -82,6 +152,7 @@ public class MainScreen extends Activity {
     private class SpeedoActionListener implements LocationListener
 
     {
+
 
 
         @Override
@@ -129,6 +200,7 @@ public class MainScreen extends Activity {
             // TODO Auto-generated method stub
 
         }
+
     }
 }
 
